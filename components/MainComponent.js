789392c535191a5c278edd3444eaeeb04fa1c6a1
Sergio Esteban Torres <esteban.towerz@gmail.com>
2019-04-18 20:44:24 -0500

    Adding Contact and About info to our Mobile App

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
 import Dishdetail from './DishdetailComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
@@ -36,6 +38,34 @@ const HomeNavigator = createStackNavigator({
     }
   })
 
+const ContactNavigator = createStackNavigator({
+  Contact: { screen: Contact }
+}, {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#0D19A3'
+      },
+      headerTintColor: '#15DB95',
+      headerTitleStyle: {
+        color: '#15DB95'
+      }
+    }
+  })
+
+const AboutNavigator = createStackNavigator({
+  About: { screen: About }
+}, {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#0D19A3'
+      },
+      headerTintColor: '#15DB95',
+      headerTitleStyle: {
+        color: '#15DB95'
+      }
+    }
+  })
+
 const MainNavigator = createDrawerNavigator({
   Home: {
     screen: HomeNavigator,
@@ -44,11 +74,25 @@ const MainNavigator = createDrawerNavigator({
       drawerLabel: 'Home'
     }
   },
+  About: {
+    screen: AboutNavigator,
+    navigationOptions: {
+      title: 'About Us',
+      drawerLabel: 'About Us',
+    }
+  },
   Menu: {
     screen: MenuNavigator,
     navigationOptions: {
       title: 'Menu',
       drawerLabel: 'Menu',
+    }
+  },
+  Contact: {
+    screen: ContactNavigator,
+    navigationOptions: {
+      title: 'Contact Us',
+      drawerLabel: 'Contact Us',
     }
   }
 }, {
